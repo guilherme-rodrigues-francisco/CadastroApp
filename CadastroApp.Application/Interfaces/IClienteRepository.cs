37bf62a8abc56aa6c79ab5db7634aeaa43a09a92
guilherme.rfrancisc2 <guilherme.rfrancisc2@SMP0582833W10-1>
2026-03-31 19:23:25 -0300

    Criação da tarefa ExcluirAsync

--- CadastroApp.Application/Interfaces/IClienteRepository.cs
+++ CadastroApp.Application/Interfaces/IClienteRepository.cs
@@ -5,4 +5,6 @@ namespace CadastroApp.Application.Interfaces;
 public interface IClienteRepository
 {
     Task<Cliente> AdicionarAsync(Cliente cliente);
+    Task<Cliente> ExcluirAsync(Cliente cliente);
+    Task<Cliente> Async(Cliente cliente);
 }
