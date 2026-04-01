813ddf68f8f40917e5da703b164450bb1bef1324
guilherme.rfrancisc2 <guilherme.rfrancisc2@SMP0582833W10-1>
2026-04-01 20:47:37 -0300

    feat: implementa interface IClienteRepository

--- CadastroApp.Application/Interfaces/IClienteRepository.cs
+++ CadastroApp.Application/Interfaces/IClienteRepository.cs
@@ -4,8 +4,28 @@ namespace CadastroApp.Application.Interfaces;
 
 public interface IClienteRepository
 {
+    /// <summary>
+    /// Adiciona um cliente ao DB
+    /// </summary>
     Task<Cliente> AdicionarAsync(Cliente cliente);
-    Task<Cliente> ExcluirAsync(Cliente cliente);
+
+    /// <summary>
+    /// Atualiza um cliente no DB
+    /// </summary>
     Task<Cliente> AtualizarAsync(Cliente cliente);
-    Task<Cliente> BuscarPorIdAsync(Cliente cliente);
+
+    /// <summary>
+    /// Exclui um cliente no DB
+    /// </summary>
+    Task<bool> ExcluirAsync(int id);
+
+    /// <summary>
+    /// Busca um cliente especifico usando o Id
+    /// </summary>
+    Task<Cliente> BuscarPorIdAsync(int id);
+
+    /// <summary>
+    /// Busca todos clientes no DB
+    /// </summary>
+    Task<List<Cliente>> ListarTodosAsync();
 }
