cdaea13198f6788bc905ee8fb9179f3bb676e38c
guilherme.rfrancisc2 <guilherme.rfrancisc2@SMP0582833W10-1>
2026-04-01 22:01:53 -0300

    feat: Implementa a interface IUsuarioRepository

--- CadastroApp.Application/Interfaces/IUsuarioRepository.cs
+++ CadastroApp.Application/Interfaces/IUsuarioRepository.cs
@@ -1,5 +1,21 @@
-﻿namespace CadastroApp.Application.Interfaces;
+﻿using CadastroApp.Domain.Entities;
 
-internal class IUsuarioRepository
+namespace CadastroApp.Application.Interfaces;
+
+public interface IUsuarioRepository
 {
+    /// <summary>
+    /// Busca um usuário pelo login (usado na autenticação) 
+    /// </summary>
+    Task<Usuario?> BuscarPorLoginAsync(string login);
+
+    /// <summary>
+    /// Adiciona um novo usuário ao DB
+    /// </summary>
+    Task AdicionarAsync(Usuario usuario);
+
+    /// <summary>
+    /// Verifica se já existe um usuário com o login informado
+    /// </summary>
+    Task<bool> LoginExisteAsync(string login);
 }
