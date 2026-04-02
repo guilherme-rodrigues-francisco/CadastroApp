b8ab4634100ba6c8ba3db93ef55e8572e3be0489
guilherme <guifrancisco2002@outlook.com>
2026-04-02 02:38:05 -0300

    feat: implementa a injeção de dependência - ClienteService

--- CadastroApp.Application/Services/ClienteService.cs
+++ CadastroApp.Application/Services/ClienteService.cs
@@ -1,5 +1,12 @@
-﻿namespace CadastroApp.Application.Services;
+﻿using CadastroApp.Application.Interfaces;
+
+namespace CadastroApp.Application.Services;
 
 public class ClienteService
 {
+    private readonly IClienteRepository _clienteRepository;
+    public ClienteService(IClienteRepository clienteRepository)
+    {
+        _clienteRepository = clienteRepository;
+    }
 }
