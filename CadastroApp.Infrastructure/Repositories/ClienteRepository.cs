e107dd5d4b8a584c60d196f786e807dc3f44c6cb
guilherme.rfrancisc2 <guilherme.rfrancisc2@SMP0582833W10-1>
2026-04-06 22:22:51 -0300

    feat: implementando o contrato AdicionarAsync()

--- CadastroApp.Infrastructure/Repositories/ClienteRepository.cs
+++ CadastroApp.Infrastructure/Repositories/ClienteRepository.cs
@@ -1,7 +1,19 @@
-﻿namespace CadastroApp.Infrastructure.Repositories
-{
-    public class ClienteRepository
-    {
+﻿using CadastroApp.Application.Interfaces;
+using CadastroApp.Domain.Entities;
 
+namespace CadastroApp.Infrastructure.Repositories;
+
+public class ClienteRepository : IClienteRepository
+{
+    private readonly string _connectionString;
+
+    public ClienteRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task<Cliente> AdicionarAsync(Cliente cliente)
+    {
+        const string sql = @"INSERT INTO Cliente (Nome, Email, Telefone, DataCadastro, Ativo) VALUES(@Nome, @Email, @Telefone, @DataCadastro, @Ativo)";
     }
 }
