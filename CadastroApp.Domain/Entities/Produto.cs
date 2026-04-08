7824fe6c91f6371b062478e3bd83d236f9d528b5
guilherme <guifrancisco2002@outlook.com>
2026-04-08 09:18:53 -0300

    feat: implementa a Entidade Produto

---
+++ CadastroApp.Domain/Entities/Produto.cs
@@ -0,0 +1,46 @@
+﻿namespace CadastroApp.Domain.Entities;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string CodigoBarra { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+    public bool Ativo { get; set; }
+
+    public Produto()
+    {
+        Ativo = true;
+    }
+
+    public List<string> Validar()
+    {
+        var erros = new List<string>();
+
+        if (string.IsNullOrEmpty(Nome))
+            erros.Add("O Nome do Produto é obrigatório!");
+        else if (Nome.Length > 100)
+            erros.Add("O Nome do Produto deve conter menos que 100 caracteres!");
+
+        if (string.IsNullOrEmpty(CodigoBarra))
+            erros.Add("O Código de Barra do Produto é obrigatório!");
+        else if (CodigoBarra.Length < 8)
+            erros.Add("O Código de Barra do Produto deve conter pelo menos 8 caracteres!");
+        else if (CodigoBarra.Length > 20)
+            erros.Add("O Código de Barra do Produto deve conter MENOS que 20 caracteres!");
+
+        if (Preco <= 0)
+            erros.Add("O Preço do Produto deve ser maior que zero!");
+
+        if (Estoque < 0)
+            erros.Add("O Estoque do Produto não pode ser negativo!");
+
+        return erros;
+    }
+
+    public bool EhValido()
+    {
+        return Validar().Count == 0;
+    }
+}
