2c30cc81b663c9012ad2cc79a61d16d384dafa8f
guilherme <guifrancisco2002@outlook.com>
2026-04-08 09:25:55 -0300

    feat: implementa a Interface IProdutoRepository

---
+++ CadastroApp.Application/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CadastroApp.Domain.Entities;
+
+namespace CadastroApp.Application.Interfaces;
+
+public interface IProdutoRepository
+{
+    Task<Produto> AdicionarAsync(Produto produto);
+    Task<Produto> AtualizarAsync(Produto produto);
+    Task<bool> ExcluirAsync(int id);
+    Task<Produto?> BuscarPorIdAsync(int id);
+    Task<Produto?> BuscarPorCodigoBarraAsync(string codigoBarra);
+    Task<List<Produto>> ListarTodosAsync();
+     Task<List<Produto>> ListarAtivosAsync();
+}
